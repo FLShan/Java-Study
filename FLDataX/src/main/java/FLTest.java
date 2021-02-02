17987fb4b6f04c1c7c59bc2742f00de03928e420
FLShan <shanfangliang@163.com>
2021-02-02 11:51:10 +0800

    -）删除项目

--- FLDataX/src/main/java/FLTest.java
+++
@@ -1,7 +0,0 @@
-public class FLTest {
-    public static void main(String[] args) {
-        System.out.println("test 1");
-        System.out.println("test 2");
-        System.out.println("test 3");
-    }
-}
